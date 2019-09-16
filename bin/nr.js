fd08c434716c0e2f4060c053770aeaa0337fd3f2
Jameswain <Jameswain@163.com>
2019-09-16 15:08:39 +0800

    添加查看当前版本参数

--- bin/nr.js
+++ bin/nr.js
@@ -10,30 +10,29 @@ const pkg = require('./package.json');
 const notifier = updateNotifier({ pkg, updateCheckInterval: 1000 * 60 * 60 * 2 });  // 2小时检查一次
 notifier.notify({ isGlobal: true });
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
-
-if (!scripts) process.exit();
-
-function searchScripts(answers, input) {
-  input = input || '';
-  return new Promise(function(resolve) {
-    const fuzzyResult = fuzzy.filter(input, scripts);
-    resolve(
-      fuzzyResult.map(function(el) {
-        return el.original;
-      })
-    );
+if (process.argv[2] && ['-v', '-version', '--version'].includes(process.argv[2])) {
+  console.log(pkg.version);
+} else if (scripts) {
+  function searchScripts(answers, input) {
+    input = input || '';
+    return new Promise(function(resolve) {
+      const fuzzyResult = fuzzy.filter(input, scripts);
+      resolve(
+        fuzzyResult.map(function(el) {
+          return el.original;
+        })
+      );
+    });
+  }
+  inquirer.prompt([
+    {
+      type: 'autocomplete',
+      name: 'job',
+      message: '请选择你要运行的命令：',
+      source: searchScripts,
+      pageSize: 10
+    },
+  ]).then(function({ job }) {
+    spawnSync(`npm`, ['run', job], {stdio: 'inherit'});
   });
 }
-
-inquirer.prompt([
-  {
-    type: 'autocomplete',
-    name: 'job',
-    message: '请选择你要运行的命令：',
-    source: searchScripts,
-    pageSize: 10
-  },
-]).then(function({ job }) {
-  spawnSync(`npm`, ['run', job], {stdio: 'inherit'});
-});
-
