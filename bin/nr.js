2123c4ea3e081f9401bf8d60b5940857e550057c
black&white <wangzhiqiang@inke.cn>
2022-04-24 11:24:43 +0800

    upgrade: 1.0.8

--- bin/nr.js
+++ bin/nr.js
@@ -13,26 +13,25 @@ inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
 if (process.argv[2] && ['-v', '-version', '--version'].includes(process.argv[2])) {
   console.log(pkg.version);
 } else if (scripts) {
-  function searchScripts(answers, input) {
-    input = input || '';
-    return new Promise(function(resolve) {
-      const fuzzyResult = fuzzy.filter(input, scripts);
-      resolve(
-        fuzzyResult.map(function(el) {
-          return el.original;
-        })
-      );
-    });
-  }
   inquirer.prompt([
     {
       type: 'autocomplete',
       name: 'job',
       message: '请选择你要运行的命令：',
-      source: searchScripts,
-      pageSize: 10
-    },
+      pageSize: 10,
+      source: (answers, input) => {
+        input = input || '';
+        return new Promise(function(resolve) {
+          const fuzzyResult = fuzzy.filter(input, scripts);
+          resolve(
+            fuzzyResult.map(function(el) {
+              return el.original;
+            })
+          );
+        });
+      },
+    }
   ]).then(function({ job }) {
-    sync(`npm`, ['run', job], {stdio: 'inherit'});
+    sync('npm', ['run', job], {stdio: 'inherit'});
   });
 }
